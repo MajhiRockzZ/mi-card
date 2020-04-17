2e49c898d785f7939d747ce46c4a0e76e6669f44
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-04-17 19:38:18 +0530

    feat: add sized box divider

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,13 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
               Card(
                 child: ListTile(
                   leading: Icon(
