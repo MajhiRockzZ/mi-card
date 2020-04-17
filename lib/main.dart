91e43244e0547c37085e42f54da1c10b3b69cc78
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-04-17 19:19:00 +0530

    feat: add style to image and text

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,29 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
+                radius: 50.0,
                 backgroundImage: AssetImage('images/sumesh.jpg'),
               ),
               Text(
                 'Sumesh Majhi',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               Text(
                 'SOFTWARE ENGINEER',
-              )
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ],
           ),
         ),
